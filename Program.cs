4897cba9c47e887bd2fce8ffdafa9abc51d17562
MrDavisCompSci <38750430+MrDavisCompSci@users.noreply.github.com>
2026-04-29 07:42:17 +0000

    done

--- Program.cs
+++ Program.cs
@@ -4,8 +4,36 @@ class Program
 {
     static void Main()
     {
-        // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+
+        Console.Write("Enter first word: ");
+        string first = Console.ReadLine();
+        Console.Write("Enter second word: ");
+        string second = Console.ReadLine();
+
+        List<char> secondList = second.ToList<char>();
+
+        bool canBeDone = true;
+
+        foreach(char c in first)
+        {
+            if (secondList.Contains(c))
+            {
+                secondList.Remove(c);
+            }
+            else
+            {
+                canBeDone = false;
+            }
+        }
+
+        if(canBeDone)
+        {
+            Console.WriteLine($"{first} can be made from {second}");
+        }
+        else
+        {
+            Console.WriteLine($"{first} cannot be made from {second}");
+        }
     }
 }
